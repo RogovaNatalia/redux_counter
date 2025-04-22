077713ba16939f1786842565f3a33720e121c3bf
Your Name <kamelia-17@mail.ru>
2025-04-22 13:40:30 +0200

    connect

--- src/index.js
+++ src/index.js
@@ -1,47 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { createStore, bindActionCreators } from "redux";
-import * as actions from "./actions";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+
 import reducer from "./reducer";
+import { Provider } from "react-redux";
+
+import App from "./components/App";
 
 const store = createStore(reducer);
-
-const { dispatch, subscribe, getState } = store;
-
-const update = () => {
-  document.getElementById("counter").textContent = getState().value;
-};
-
-subscribe(update);
-
-// const bindActionCreator =
-//   (creator, dispatch) =>
-//   (...args) => {
-//     dispatch(creator(...args));
-//   };
-
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-document.getElementById("inc").addEventListener("click", inc);
-
-document.getElementById("dec").addEventListener("click", dec);
-
-document.getElementById("rnd").addEventListener("click", () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
-
-// let state = reducer(initialState, { type: "INC" });
-// state = reducer(state, { type: "INC" });
-// state = reducer(state, { type: "INC" });
-// state = reducer(state, { type: "INC" });
-// console.log(state);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <></>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
