c898215d633cb0c92f4c36b5ecd851b25244562c
Your Name <kamelia-17@mail.ru>
2025-04-22 11:58:02 +0200

    Оптимизация через actionCreators и bindActionCreator

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, bindActionCreators } from "redux";
+import * as actions from "./actions";
+import reducer from "./reducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(reducer);
+
+const { dispatch, subscribe, getState } = store;
+
+const update = () => {
+  document.getElementById("counter").textContent = getState().value;
+};
+
+subscribe(update);
+
+// const bindActionCreator =
+//   (creator, dispatch) =>
+//   (...args) => {
+//     dispatch(creator(...args));
+//   };
+
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+// const decDispatch = bindActionCreators(dec, dispatch);
+// const rndDispatch = bindActionCreators(rnd, dispatch);
+
+document.getElementById("inc").addEventListener("click", inc);
+
+document.getElementById("dec").addEventListener("click", dec);
+
+document.getElementById("rnd").addEventListener("click", () => {
+  const value = Math.floor(Math.random() * 10);
+  rnd(value);
+});
+
+// let state = reducer(initialState, { type: "INC" });
+// state = reducer(state, { type: "INC" });
+// state = reducer(state, { type: "INC" });
+// state = reducer(state, { type: "INC" });
+// console.log(state);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <></>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
